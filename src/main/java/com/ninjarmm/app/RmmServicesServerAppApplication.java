472c36847c9e1982bec4665c4b98754da3410ed0
juanferborjab <juanferborja@gmail.com>
2022-01-25 00:58:42 -0300

    First Commit

---
+++ src/main/java/com/ninjarmm/app/RmmServicesServerAppApplication.java
@@ -0,0 +1,13 @@
+package com.ninjarmm.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmmServicesServerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RmmServicesServerAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ninjarmm/app/RmmServicesServerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ninjarmm.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmmServicesServerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
